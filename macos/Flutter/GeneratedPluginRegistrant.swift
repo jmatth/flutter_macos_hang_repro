0b30602d0e1488e15cfb56450c17999cf0e0b3f5
Joshua Matthews <josh@jmatth.com>
2022-05-24 22:04:21 -0400

    Adding contextual_menu plugin

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,8 @@
 import FlutterMacOS
 import Foundation
 
+import contextual_menu
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ContextualMenuPlugin.register(with: registry.registrar(forPlugin: "ContextualMenuPlugin"))
 }
